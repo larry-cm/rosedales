d828e777fc609fca30bd061568998c373c9c97a8
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2025-10-06 21:37:21 -0500

    prinicoal

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,48 @@
+import type React from "react";
+import { useState } from "react";
+import { ChevronLeft,ChevronRight } from "react-feather";
+
+export default function Carousel({arrayL,children:slides}:{arrayL:number,children?:React.ReactElement[]}){
+
+  const [position,setPosition] = useState(0)
+
+  const nextPosition= () =>setPosition(p => slides && p === arrayL - 1 ? 0 : p+1 )
+  const prevPosition= () =>setPosition(p => slides && p === 0 ? arrayL - 1 : p-1)
+console.log(slides);
+
+  return (
+    <div className="overflow-hidden relative bg-green-500">
+      <div className="flex transition duration-300 ease-in-out"
+        style={{
+          transform:`translateX(-${position * 100}%)`
+        }}
+      >
+        {slides}
+      </div>
+
+      <div className="absolute inset-0 flex items-center justify-between p-4">
+        <button onClick={prevPosition} className="cursor-pointer transition duration-300 bg-white/80 p-2 rounded-full shadow  hover:bg-white">
+<ChevronLeft size={24}  stroke="black" strokeWidth="1.5"/>
+        </button>
+        <button onClick={nextPosition} className="cursor-pointer transition duration-300 bg-white/80 p-2 rounded-full shadow  hover:bg-white">
+<ChevronRight size={24} stroke="black"  strokeWidth="1.5"/>
+        </button>
+      </div>
+
+      <div className="absolute bottom-4 right-0 left-0">
+        <div className="flex  items-center justify-center gap-2" >
+{
+ slides &&  slides?.map((_,i) => (
+    <div className={
+      `transition-all size-3 bg-white rounded-full ${position === i ?"p-2 " :"bg-opacity-50" }`
+      }>
+
+    </div>
+  ))
+}
+        </div>
+        
+      </div>
+    </div>
+  )
+}
