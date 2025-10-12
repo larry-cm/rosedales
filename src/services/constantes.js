df43fccb5d45f228f00fca3799d97242dd6c4dcf
larry-cm <larryycel@gmail.com>
2025-10-12 04:03:50 -0600

    optimizando

---
+++ src/services/constantes.js
@@ -0,0 +1,20 @@
+// constantes del header
+export const LINK_PAGE = ["Eventos", "Promociones", "Locales", "Contáctanos"]
+export const SOCIAL_MEDIA = ["Facebook", "Instagram", "Linkedin"]
+export const URL_LOGO = "https://canaveral.com.co/wp-content/uploads/2024/10/logo-cana.jpeg"
+// constantes del carrusel
+export const IMAGES = [
+    { url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2", alt: "Imagen 1" },
+    {
+        url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2",
+        alt: "Imagen 2"
+    },
+    {
+        url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2",
+        alt: "Imagen 3"
+    },
+    {
+        url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2",
+        alt: "Imagen 4"
+    }
+]
