e3b380101f79994047b50a6f01f7afa4b4df3d09
larry-cm <larryycel@gmail.com>
2025-10-14 07:33:41 -0600

    optimizando las constantes

--- src/services/constantes.js
+++ src/services/constantes.js
@@ -1,20 +1,16 @@
 // constantes del header
-export const LINK_PAGE = ["Eventos", "Promociones", "Locales", "Contáctanos"]
-export const SOCIAL_MEDIA = ["Facebook", "Instagram", "Linkedin"]
+export const LINK_PAGE = ["Eventos", "Promociones", "Locales", "Contáctenos"]
+export const SOCIAL_MEDIA = ["Facebook", "Instagram", "LinkedIn"]
 export const URL_LOGO = "https://canaveral.com.co/wp-content/uploads/2024/10/logo-cana.jpeg"
 // constantes del carrusel
-export const IMAGES = [
-    { url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2", alt: "Imagen 1" },
-    {
-        url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2",
-        alt: "Imagen 2"
-    },
-    {
-        url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2",
-        alt: "Imagen 3"
-    },
-    {
-        url: "https://media1.thrillophilia.com/filestore/n2ib9inwzcilxpg3aumbigvq4jus_IMG_World_Dubai_Fun_38a0986c1a.jpg?w=400&dpr=2",
-        alt: "Imagen 4"
-    }
-]
+const URLS_IMAGES = new Array(10).fill("/image.webp")
+const ALTERNATIVE_TEXT_IMAGES = new Array(10).fill(null).map((_, i) => `Imagen del local numero ${i} en el centro comercial`)
+const SLUGS_IMAGES = new Array(10).fill(null).map((_, i) => `local-${i + 1}`)
+
+export const IMAGES = [URLS_IMAGES, ALTERNATIVE_TEXT_IMAGES, SLUGS_IMAGES].map((_, i) =>
+({
+    url: URLS_IMAGES[i],
+    alt: ALTERNATIVE_TEXT_IMAGES[i],
+    slug: SLUGS_IMAGES[i],
+})
+)
