71bd2484c3bb546d7b9bae6de0d084929db4e818
larry-cm <larryycel@gmail.com>
2025-10-18 08:56:21 -0600

    mejorando el diseño de los botones del carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -28,11 +28,11 @@ export default function Carousel({
       </div>
 
       <div className="absolute inset-0 flex items-center justify-between p-4">
-        <button onClick={handlePrevClick} className="focus:ring ring-primary cursor-pointer   rounded-full shadow">
-          <ChevronLeft className="block btn-effect bg-white/70 backdrop-blur-md rounded-full md:size-12 stroke-primary  transition duration-300" size={24} strokeWidth="1.5" />
+        <button onClick={handlePrevClick} className="focus:ring ring-green-500 cursor-pointer rounded-full shadow">
+          <ChevronLeft className="block btn-effect hover:bg-white/70 backdrop-blur-md bg-green-500 stroke-white rounded-full size-8 sm:10 md:size-12 hover:stroke-green-500  transition duration-300" size={24} strokeWidth="2" />
         </button>
-        <button onClick={handleNextClick} className="focus:ring ring-primary cursor-pointer   rounded-full shadow">
-          <ChevronRight className="block btn-effect bg-white/70 backdrop-blur-md rounded-full md:size-12 stroke-primary scale-100 transition duration-300" size={24} strokeWidth="1.5" />
+        <button onClick={handleNextClick} className="focus:ring ring-green-500 cursor-pointer rounded-full shadow">
+          <ChevronRight className="block btn-effect hover:bg-white/70 backdrop-blur-md bg-green-500 stroke-white rounded-full size-8 sm:10 md:size-12 hover:stroke-green-500 scale-100 transition duration-300" size={24} strokeWidth="2" />
         </button>
       </div>
 
